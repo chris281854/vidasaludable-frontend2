46e94ee83be887bb163298d9630084c476fc2a03
chris281854 <christopherajimenez88@gmail.com>
2024-10-17 20:42:47 -0400

    fronted

---
+++ src/app/paciente/page.tsx
@@ -0,0 +1,11 @@
+const PacienteDashboard = () => {
+    return (
+        <div className=" bg-slate-300 w-full h-screen">
+              <div className="flex mt-6  w-[1278px] h-3.5 bg-[#25aa80]">
+              </div>
+
+
+       </div>
+    );
+    }
+    export default PacienteDashboard;
